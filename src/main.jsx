7cbe1c1f504c3a676870382cc0df320df10372b5
Priya Kumari <chaudhary21priya@gmail.com>
2025-08-14 12:39:10 +0530

    feat: setup React Router with initial pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./components/Login/Login.jsx";
+import Error from "./components/Error/Error.jsx";
+import Main from "./components/Main/Main.jsx";
 
-createRoot(document.getElementById('root')).render(
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Main />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+    errorElement: <Error />
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={appRouter} />
+  </StrictMode>
+);
